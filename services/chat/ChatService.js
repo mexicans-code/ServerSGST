d2f87a15f5c9bb205a00bf64ec6311ca6ca7ff0b
Ricardo <rica82561@gmail.com>
2025-11-21 13:52:03 -0600

    update chat

--- services/chat/ChatService.js
+++ services/chat/ChatService.js
@@ -19,7 +19,7 @@ const supabase = createClient(
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://sgst-production.up.railway.app",
         methods: ["GET", "POST"],
         credentials: true
     },
